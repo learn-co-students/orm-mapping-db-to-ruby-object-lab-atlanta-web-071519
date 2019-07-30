02796529d3c1075db450266433686b049fb7f793
Raza <rtshareef@gmail.com>
2019-07-30 11:20:32 -0400

    Done.

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -96,6 +96,7 @@ describe Student do
 
         all_in_9 = Student.all_students_in_grade_9
         expect(all_in_9.size).to eq(1)
+        expect(all_in_9.first.name).to eq("Sam")
       end
     end
 
